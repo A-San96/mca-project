26585c0c5e839df743337ab0d52cf29a4f15c0cd
Assane DIATTA <assane.diatta@awaya.ca>
2021-04-08 13:32:03 +0000

    new structure for the project

--- main.py
+++ main.py
@@ -1,7 +1,68 @@
+from typing import List
+
+import databases
+import sqlalchemy
 from fastapi import FastAPI
 
-app = FastAPI()
+#from .models import *
+from schemas import City
+
+
+# SQLAlchemy specific code, as with any other app
+DATABASE_URL = "mysql://root:diatta96@localhost/sakila"
+
+database = databases.Database(DATABASE_URL)
+
+
+
+db = [
+  {
+    "name": "Dakar",
+    "timezone": "Senegal/Dakar"
+  },
+  {
+    "name": "Ziguinchor",
+    "timezone": "Senegal/Ziguinchor"
+  },
+  {
+    "name": "Thies",
+    "timezone": "Senegal/Thies"
+  }
+]
+
+
+app = FastAPI(title="Google Meet Analyzer API")
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 
 @app.get('/')
-def index():
-    return {'msg': 'Hello World'}
+async def index():
+    return await {'key': 'value'}
+
+@app.get('/cities')
+async def get_cities():
+    return await db
+
+@app.get('/cities/{city_id}')
+async def get_city(city_id : int):
+    return await db[city_id-1]
+
+@app.post('/cities')
+async def create_city(city: City):
+    db.append(city.dict())
+    return await db[-1]
+
+@app.delete('/cities/{city_id}')
+async def delete_city(city_id: int):
+    db.pop(city_id-1)
+    return await {}
+
+
